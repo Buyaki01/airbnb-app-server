ce1f939973486861bde223c31d9f506271f7668e
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-27 13:16:37 +0300

    Add logger middleware

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
 const PORT = process.env.PORT || 5000
+
+app.use(logger)
+
+app.use(express.json())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 

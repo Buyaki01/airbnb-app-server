fb58a4d5bad6e483eedb5551f2c901319c068677
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-27 12:05:21 +0300

    Add style.css, routes/root and server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', require('./routes/root'))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))

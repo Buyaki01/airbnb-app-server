f05082e5cd59f358023cf45837596b2467700188
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-29 12:54:39 +0300

    Add structure of auth controller

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
-app.use('/users', require('./routes/registerRoutes'))
+app.use('/register', require('./routes/registerRoutes'))
 app.use('/auth', require('./routes/authRoutes'))
 app.use('/accommodations', require('./routes/accommodationRoutes'))
 app.use('/uploaded-photo', require('./routes/photoRoutes'))

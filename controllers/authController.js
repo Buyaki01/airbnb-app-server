8a2b5586754093e4251fd2a151ad0242590c485d
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-29 15:42:56 +0300

    Add verifyJWT middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,13 +4,13 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 
 const login = asyncHandler(async (req, res) => {
-  const { username, password } = req.body
+  const { email, password } = req.body
 
-  if (!username || !email || !password) {
+  if (!email || !password) {
     return res.status(400).json({ message: 'All fields are required' })
   }
 
-  const foundUser = await User.findOne({ username }).exec()
+  const foundUser = await User.findOne({ email }).exec()
 
   if (!foundUser) {
     return res.status(401).json({ message: 'Unauthorized'})
--- routes/accommodationRoutes.js
+++ routes/accommodationRoutes.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const accommodationsController = require('../controllers/accommodationsController')
+const verifyJWT = require('../middleware/verifyJWT')
 
 router.route('/')
   .get(accommodationsController.getAllAccommodations)
   .delete()
 
 router.route('new')
-  .post(accommodationsController.createNewAccommodation)
+  .post(verifyJWT, accommodationsController.createNewAccommodation)
 
 router.route('/:id')
   .get(accommodationsController.getSpecificAccommodation)
-  .patch(accommodationsController.updateAccommodation)
+  .patch(verifyJWT, accommodationsController.updateAccommodation)
 
 router.route('/myaccommodations')
-  .get(accommodationsController.getAllAccommodationsForOwner)
+  .get(verifyJWT, accommodationsController.getAllAccommodationsForOwner)
 
 module.exports = router

b615fc3eaa64913cd56dced4761905e4c694c29c
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-13 15:39:34 +0300

    Add getAllBookings to be a protected route

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,12 +3,10 @@ const router = express.Router()
 const bookingsController = require('../controllers/bookingsController')
 const verifyJWT = require('../middleware/verifyJWT')
 
-router.route('/')
-  .get(bookingsController.getAllBookings)
-  
 router.use(verifyJWT)
 
 router.route('/')
+  .get(bookingsController.getAllBookings)
   .get(bookingsController.getAllBookingsByOwner)
   .post(bookingsController.createNewBooking)
   .get(bookingsController.getSpecificBooking)

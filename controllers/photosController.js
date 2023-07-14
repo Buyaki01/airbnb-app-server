b08e5c16c7441612af560bffef0366c202ba908d
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-14 16:59:02 +0300

    Update uploadPhotoFromDevice method

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -1,8 +1,8 @@
 const asyncHandler = require('express-async-handler')
 const downloadImage = require('image-downloader')
 const multer  = require('multer')
-const upload = multer({ dest: 'public/images/' })
 const path = require('path')
+const upload = multer({ dest: path.join(__dirname, '..', 'public', 'images') })
 
 const uploadPhotoByLink = asyncHandler(async (req, res) => {
   const { link } = req.body
@@ -23,24 +23,38 @@ const uploadPhotoByLink = asyncHandler(async (req, res) => {
     })
 
     res.json(newName)
-    
+
   } catch (error) {
     res.status(500).json({ error: 'Failed to upload photo from link' })
   }
 })
 
-const uploadPhotoFromDevice = (upload.array('photos', 100), (req, res) => {
-  const uploadedPhotos = []
-  for (let i = 0; i < req.files.length; i++) {
-    const {originalname} = req.files[i]
-    const fileExtension = path.extname(originalname)
-    const newPath =  req.files[i].path + fileExtension
-    
-    fs.renameSync(req.files[i].path, newPath)
-    uploadedPhotos.push(newPath)
-  }
-  res.json(uploadedPhotos)
-})
+const uploadPhotoFromDevice = (req, res) => {
+  const upload = upload.array('photos', 100) // Assuming 'upload' is defined and correctly configured
+
+  upload(req, res, err => {
+    if (err) {
+      // Handle any upload errors
+      console.error(err)
+      return res.status(500).json({ error: 'Failed to upload photos' })
+    }
+
+    const uploadedPhotos = []
+
+    console.log(req.files)
+
+    for (let i = 0; i < req.files.length; i++) {
+      const { originalname } = req.files[i]
+      const fileExtension = path.extname(originalname)
+      const newPath = req.files[i].path + fileExtension
+
+      fs.renameSync(req.files[i].path, newPath)
+      uploadedPhotos.push(newPath)
+    }
+
+    res.json(uploadedPhotos)
+  })
+}
 
 module.exports = {
   uploadPhotoByLink,

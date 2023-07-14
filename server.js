9f15b40672650cdbdcbe433e77f73bd292f72709
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-14 14:55:02 +0300

    Update middlware to access images in the public folder

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(express.json())
 
 app.use(cookieParser())
 
+app.use('/images', express.static(path.join(__dirname, 'public', 'images')))
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))

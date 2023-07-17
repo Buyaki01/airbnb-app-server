14cf7ff081357fa0d6c4273cd2a14836cb5c19f3
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-17 09:56:43 +0300

    Require fs in photosController

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -2,6 +2,8 @@ const asyncHandler = require('express-async-handler')
 const downloadImage = require('image-downloader')
 const multer  = require('multer')
 const path = require('path')
+const fs = require('fs')
+
 const upload = multer({ dest: path.join(__dirname, '..', 'public', 'images') })
 
 const uploadPhotoByLink = asyncHandler(async (req, res) => {
@@ -30,29 +32,26 @@ const uploadPhotoByLink = asyncHandler(async (req, res) => {
 })
 
 const uploadPhotoFromDevice = (req, res) => {
-  const upload = upload.array('photos', 100) // Assuming 'upload' is defined and correctly configured
-
-  upload(req, res, err => {
+  upload.array('photos', 100)(req, res, (err) => {
     if (err) {
-      // Handle any upload errors
-      console.error(err)
+      console.error(err);
       return res.status(500).json({ error: 'Failed to upload photos' })
     }
 
-    const uploadedPhotos = []
+    if (!req.files || req.files.length === 0) {
+      return res.status(400).json({ error: 'No photos uploaded' })
+    }
 
-    console.log(req.files)
-
+    const uploadedFiles = [];
     for (let i = 0; i < req.files.length; i++) {
       const { originalname } = req.files[i]
       const fileExtension = path.extname(originalname)
       const newPath = req.files[i].path + fileExtension
 
-      fs.renameSync(req.files[i].path, newPath)
-      uploadedPhotos.push(newPath)
+      fs.renameSync(req.files[i].path, newPath);
+      uploadedFiles.push(newPath.replace('images\\', ''))
     }
-
-    res.json(uploadedPhotos)
+    res.json(uploadedFiles)
   })
 }
 
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/', require('./routes/root'))
 app.use('/register', require('./routes/registerRoutes'))
 app.use('/auth', require('./routes/authRoutes'))
 app.use('/accommodations', require('./routes/accommodationRoutes'))
-app.use('/photo', require('./routes/photoRoutes'))
+app.use('/upload', require('./routes/photoRoutes'))
 app.use('/profile', require('./routes/profileRoutes'))
 app.use('/bookings', require('./routes/bookingRoutes'))
 
